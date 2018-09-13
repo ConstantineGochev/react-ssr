3acfb014f5760597e6f1c4210db56578f550d551
Constantin <constantin@constantine-pc.zori.local>
2018-09-13 16:38:08 +0300

    added docker to project2

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ mongoose.connect(
   'mongodb://mongo:27017/platform',
   { useNewUrlParser: true },
   function (err, db) {
-   // db.collection('boats').insertMany([{ model: 'Boat_1' }, { model: 'Boat_2' }, { model: 'Boat_3' }])
+    if(err) {
+      console.log(err)
+    }
+    db.collection('boats').insertMany([{ model: 'Boat_1' }, { model: 'Boat_2' }, { model: 'Boat_3' }])
 })
 
 
