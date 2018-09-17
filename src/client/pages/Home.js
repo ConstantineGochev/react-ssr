d654d9bb077fcbfcb8d62e31dc292cb42258d51d
Constantin <constantin@constantine-pc.zori.local>
2018-09-17 14:50:00 +0300

    hasMounted property in the state of the app component

--- src/client/pages/Home.js
+++ src/client/pages/Home.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import {Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Button } from 'semantic-ui-react'
 
 class Home extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            hasMounted: false
+        }
+    }
+    componentDidMount () {
+        this.setState({hasMounted: true})
+    }
     render() {
         return (
             <div>
-               <h1> TEST TITLE </h1>
-               <Button>Click</Button>
-               <Link className="btn white-text text-darken-2" to='/boats' >Boats</Link>
-           </div>
-               );
-   }
+                <h1> TEST TITLE </h1>
+                <Button>Click</Button>
+               {this.state.hasMounted && <Link className="btn white-text text-darken-2" to='/boats' >Boats</Link> }
+            </div>
+        );
+    }
 }
 
 export default {
-    component:Home
+    component: Home
 }
